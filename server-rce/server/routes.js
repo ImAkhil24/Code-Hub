af64b3794d2cb7df99a24eec1f2a88f986ae1fc4
Akhil Badoni <akhilbadoni24@gmail.com>
2023-05-29 11:48:56 +0530

    deleted example route

--- server-rce/server/routes.js
+++ server-rce/server/routes.js
@@ -4,7 +4,6 @@ import linkRouter from "./api/controllers/Links/router";
 import codeRouter from "./api/controllers/code/router"; 
 
 export default function routes(app) {
-  app.use("/api/v1/examples", examplesRouter);
   app.use("/login", authRouter);
   app.use("/link", linkRouter);
   app.use("/code", codeRouter);
