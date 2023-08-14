ab26df6030f41aa0474eb350f4e004516fbdcf9b
HawkEye201 <imt_2019024@iiitm.ac.in>
2023-08-14 23:49:34 +0530

    homepage ui changes

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,9 +2,10 @@ import React from "react";
 import Header from "./Header";
 import styles from "./styles/home.module.css";
 import Typist from "react-typist";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import BottomFooter from "./BottomFooter";
 import Terminal from "./Terminal";
+import BG from "../../assets/bg.svg";
 
 const Home = () => {
   return (
@@ -17,21 +18,37 @@ const Home = () => {
               initial={{ x: "-100vw" }}
               animate={{ x: 0 }}
               transition={{ duration: 2 }}
-               whileHover={{
-    scale: 0.8,
-    transition: { duration: 1 },
-  }}
-  >
-              Code
-              <span>Hub</span>
+              whileHover={{
+                scale: 0.8,
+                transition: { duration: 1 },
+              }}
+              style={{
+                // background: "red",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                width: "100%",
+              }}
+            >
+              <img
+                src={BG}
+                style={{
+                  width: "25rem",
+                  height: "25rem",
+                }}
+              />
+              <div>
+                Code
+                <span className={styles.heading}>Hub</span>
+              </div>
             </motion.h1>
             <Typist startDelay={2} avgTypingDelay={120}>
               Faster, Lighter and Better IDE for everyone...
             </Typist>
-        </div>
-        <div className={styles.right}>
-        <Terminal />
-        </div>
+          </div>
+          <div className={styles.right}>
+            <Terminal />
+          </div>
         </div>
         <BottomFooter />
       </div>
